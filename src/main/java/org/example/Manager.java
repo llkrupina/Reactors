58a241070da6ecde9d48cfe51d5528ff128d566c
llkrupina <kwaifare@mail.ru>
2024-04-29 12:38:21 +0300

    four

--- src/main/java/org/example/Manager.java
+++ src/main/java/org/example/Manager.java
@@ -4,9 +4,9 @@ import java.io.File;
 import java.util.Map;
 
     public class Manager {
-        private final FileReader jsonReader;
-        private final FileReader xmlReader;
-        private final FileReader yamlReader;
+        private final JSONReader jsonReader;
+        private final XMLReader xmlReader;
+        private final YAMLReader yamlReader;
         private final ReactorStorage reactorStorage;
 
         public Manager() {
